5b1b9d22d2841058c1358111ce5f2fcfb438c564
danlagos <lagos.d@gmail.com>
2019-04-08 16:09:45 -0700

    used post command to get dummy data from axios.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Home from './components/Home'
 import About from './components/About'
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { Link, NavLink, withRouter } from 'react-router-dom'
+import { withRouter } from 'react-router-dom'
 import { BrowserRouter } from 'react-router-dom'
 
 const Navbar = (props) => {
-  console.log(props);
-  setTimeout(() =>{
-    props.history.push('/about')
-  }, 2000)
+  // console.log(props);
+  // setTimeout(() =>{
+  //   props.history.push('/about')
+  // }, 2000)
   return(
     <nav className="nav-wrapper red darken-3">
       <div className="container">
